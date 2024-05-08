bfff6d52cac5f0de1109790b7a2f94fc025a182b
ksskol <skolotakristina2.com>
2024-05-08 02:44:37 +0100

    css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,14 @@
 import { Link } from "react-router-dom";
 
 export default function Header() {
-    return(
-        <header>
-            <div className="header-nav">
-            <button>Account</button>
-            <Link to={`/`} className="link">
+  return (
+    <header>
+      <div className="header-nav">
+        <button>Account</button>
+          <Link to={`/`} className="link">
             <h2>NC News</h2>
-            </Link>
-            </div>
-        </header>
-    )
+          </Link>
+      </div>
+    </header>
+  );
 }
